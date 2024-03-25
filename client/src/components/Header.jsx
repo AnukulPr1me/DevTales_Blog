a7b7693e741e40c64e15073a52b07bc39ffe61cb
AnukulPr1me <anukul_chandra@outlook.com>
2024-03-25 17:21:52 +0530

    added dark mode

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,11 +11,14 @@ import {
 } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const {theme} = useSelector((state) => state.theme);
   return (
     <Navbar className="border-b-2">
       <Link
@@ -40,8 +43,13 @@ export default function Header() {
       </Button>
 
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden lg:inline" color="gray" pill>
-          <FaMoon />
+        <Button
+          className="w-12 h-10 hidden lg:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun/> : <FaMoon/>}
         </Button>
         {currentUser ? (
           <Dropdown
@@ -52,13 +60,15 @@ export default function Header() {
             }
           >
             <Dropdown.Header>
-              <span className='block text-sm'>@{currentUser.username}</span>
-              <span className='block text-medium truncate'>{currentUser.email}</span>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-medium truncate">
+                {currentUser.email}
+              </span>
             </Dropdown.Header>
-            <Link to={'/dashboard?tab=profile'}>
+            <Link to={"/dashboard?tab=profile"}>
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
-            <DropdownDivider/>
+            <DropdownDivider />
             <Dropdown.Item>Sign Out</Dropdown.Item>
           </Dropdown>
         ) : (
